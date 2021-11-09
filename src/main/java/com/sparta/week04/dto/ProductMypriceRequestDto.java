097a13ae4e92619ee68141a39082db7eac3d8ec3
김성훈 <shkim11997@gmail.com>
2021-11-10 00:17:37 +0900

    Initial commit

---
+++ src/main/java/com/sparta/week04/dto/ProductMypriceRequestDto.java
@@ -0,0 +1,12 @@
+package com.sparta.week04.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class ProductMypriceRequestDto {
+    private int myprice;
+}
