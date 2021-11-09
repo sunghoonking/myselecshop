d76fd7fa87440d4c02d56f5f5fe3be67a6fc00af
sunghoonkim <90130141+sunghoonking@users.noreply.github.com>
2021-11-10 08:58:49 +0900

    Update Timestamped.java

--- src/main/java/com/sparta/week04/models/Timestamped.java
+++ src/main/java/com/sparta/week04/models/Timestamped.java
@@ -9,7 +9,7 @@ import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
-@Getter // get 함수를 자동 생성합니다.
+@Getter 
 @MappedSuperclass // 멤버 변수가 컬럼이 되도록 합니다.
 @EntityListeners(AuditingEntityListener.class) // 변경되었을 때 자동으로 기록합니다.
 public abstract class Timestamped {
